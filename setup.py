34342b6db7f943577e8e8a3ae9dbd4fac4217153
Joerg Schultze-Lutter <joerg.schultze.lutter@gmail.com>
2025-09-25 20:21:03 +0200

    Updates

--- setup.py
+++ setup.py
@@ -5,13 +5,39 @@ import os
 import re
 
 VERSION_REGEX = r'__version__\s*=\s*"(.*)"'
-MY_SOURCE_DIR = "src"
 
+PACKAGE_SOURCE_DIR = "src"
+
+PACKAGE_NAME="my package name"
+DESCRIPTION = "my description"
+AUTHOR = "author_name"
+AUTHOR_EMAIL = "author@email"
+URL = "https://www.url.com/to/my/repository"
+# https://pypi.org/classifiers/
+CLASSIFIERS = [
+    "Intended Audience :: Developers",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Topic :: Software Development",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Operating System :: OS Independent",
+    "Development Status :: 4 - Beta",
+    "Framework :: Robot Framework",
+]
+INSTALL_REQUIRES=[
+    "my_packet_1",
+    "my_packet_2",
+]
+KEYWORDS=[
+    "My Keyword 1",
+    "My Keyword 2",
+]
+LICENSE="GNU General Public License v3 (GPLv3)"
 
 if __name__ == "__main__":
-    # get README gnd use as long description
+    # get README and use it as long description
     with open("README.md", "r") as fh:
-        long_description = fh.read()
+        LONG_DESCRIPTION = fh.read()
 
     # get VERSION value from Github workflow and terminate workflow if value is None
     GITHUB_LABEL_VERSION = os.getenv("GITHUB_LABEL_VERSION")
@@ -20,45 +46,40 @@ if __name__ == "__main__":
 
     version_from_file = None
 
-    # get version info from version file
-    with open("_version.py", "r") as fh:
-        version_file_content = fh.read()
-        matches = re.findall(VERSION_REGEX, version_file_content,re.IGNORECASE)
-        if not matches:
-            raise ValueError("Did not find version info in _version.py")
-        try:
-            version_from_file = matches[0]
-        except IndexError:
-            raise ValueError("Did not find version info in _version.py")
+    # get version info from a version file
+    # amend path to the version file if necessary
+    try:
+        with open("_version.py", "r") as fh:
+            version_file_content = fh.read()
+    except FileNotFoundError:
+        raise ValueError("Did not find _version.py")
 
-        if version_from_file != GITHUB_LABEL_VERSION:
-            raise ValueError(f"Version info from _version.py '{version_from_file}' differs from GitHub label version '{GITHUB_LABEL_VERSION}'")
+    matches = re.findall(VERSION_REGEX, version_file_content,re.IGNORECASE)
+    if not matches:
+        raise ValueError("Did not find version info in _version.py")
+    try:
+        version_from_file = matches[0]
+    except IndexError:
+        raise ValueError("Did not find version info in _version.py")
+
+    if version_from_file != GITHUB_LABEL_VERSION:
+        raise ValueError(f"Version info from _version.py '{version_from_file}' differs from GitHub label version '{GITHUB_LABEL_VERSION}'")
 
     # Amend with your project information
     setup(
-        name="my package name",
+        name=PACKAGE_NAME,
         version=VERSION,
-        description="my description",
-        long_description=long_description,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
         long_description_content_type="text/markdown",
-        author="My Name",
-        author_email="My email addresse",
-        url="URL to my repo",
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        url=URL,
         packages=find_packages(where=MY_SOURCE_DIR),
         include_package_data=True,
-        # https://pypi.org/classifiers/
-        classifiers=[
-            "Intended Audience :: Developers",
-            "Programming Language :: Python",
-            "Programming Language :: Python :: 3",
-            "Topic :: Software Development",
-            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-            "Operating System :: OS Independent",
-            "Development Status :: 4 - Beta",
-            "Framework :: Robot Framework",
-        ],
-        license="GNU General Public License v3 (GPLv3)",
-        install_requires=["my_packet_1", "my_packet_2"],
-        keywords=["My Keyword 1", "My Keyword 2"],
+        classifiers=CLASSIFIERS,
+        license=LICENSE,
+        install_requires=INSTALL_REQUIRES,
+        keywords=KEYWORDS,
     )
 
