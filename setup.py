3ae6bf2310b17ceca43b7aedbab0b169da2abda8
Joerg Schultze-Lutter <76180229+joergschultzelutter@users.noreply.github.com>
2025-09-29 16:45:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,40 +4,75 @@ from setuptools import setup, find_packages
 import os
 import re
 
+# source path for the class' package. Amend if necessary
+PACKAGE_SOURCE_DIR = "path/to/class/directorz"
+
+# Amend this section with your custom data
+PACKAGE_NAME="package-name"
+DESCRIPTION = "package-description"
+AUTHOR = "author-name"
+AUTHOR_EMAIL = "author@email.com"
+URL = "https://www.url.com/to/my/repository"
+# https://pypi.org/classifiers/
+CLASSIFIERS = [
+    "Intended Audience :: Developers",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Topic :: Software Development",
+    "Operating System :: OS Independent",
+    "Development Status :: 4 - Beta",
+    "Framework :: Robot Framework",
+]
+INSTALL_REQUIRES=[
+    "package_1", 
+    "package_2"
+]
+KEYWORDS=[
+    "Notifications", 
+    "Notification Service", 
+    "Push Notifications", 
+    "Notifier", 
+    "Alerts", 
+    "Robot Framework"
+]
+LICENSE="GNU General Public License v3 (GPLv3)"
+
+def running_in_a_github_action():
+    return os.getenv("GITHUB_ACTIONS") == "true"
+
 if __name__ == "__main__":
-    # get README gnd use as long description
+    # get README and use it as long description
     with open("README.md", "r") as fh:
-        long_description = fh.read()
+        LONG_DESCRIPTION = fh.read()
 
-    # get VERSION value from Github workflow and terminate workflow if value is None
-    VERSION = os.getenv("GITHUB_PROGRAM_VERSION")
-    if not VERSION:
-        raise ValueError("Did not receive version info from GitHub")
-
-    # Amend with your project information
+    GITHUB_PROGRAM_VERSION = ""
+    
+    if running_in_a_github_action():
+        # Only run this branch if we are part of a Github action. Otherwise, just skip
+        # it as we won't be able to get the version info from Github anyway
+        #
+        # get VERSION value from Github workflow and terminate workflow if value is None
+        GITHUB_PROGRAM_VERSION = os.getenv("GITHUB_PROGRAM_VERSION")
+        if not GITHUB_PROGRAM_VERSION:
+            raise ValueError("Did not receive release label version info from GitHub")
+    else:
+        if len(GITHUB_PROGRAM_VERSION == 0):
+            raise ValueError("Manual run requires a manually set GITHUB_PROGRAM_VERSION; change setup.py accordingly")
+    
+    # Main setup branch
     setup(
-        name="my package name",
-        version=VERSION,
-        description="my description",
-        long_description=long_description,
+        name=PACKAGE_NAME,
+        version=GITHUB_PROGRAM_VERSION,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
         long_description_content_type="text/markdown",
-        author="My Name",
-        author_email="My email addresse",
-        url="URL to my repo",
-        packages=find_packages(),
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        url=URL,
+        packages=find_packages(where=PACKAGE_SOURCE_DIR),
         include_package_data=True,
-        classifiers=[
-            "Intended Audience :: Developers",
-            "Programming Language :: Python",
-            "Programming Language :: Python :: 3",
-            "Topic :: Software Development",
-            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-            "Operating System :: OS Independent",
-            "Development Status :: 4 - Beta",
-            "Framework :: Robot Framework",
-        ],
-        license="GNU General Public License v3 (GPLv3)",
-        install_requires=["my_packet_1", "my_packet_2"],
-        keywords=["My Keyword 1", "My Keyword 2"],
+        classifiers=CLASSIFIERS,
+        license=LICENSE,
+        install_requires=INSTALL_REQUIRES,
+        keywords=KEYWORDS,
     )
-
